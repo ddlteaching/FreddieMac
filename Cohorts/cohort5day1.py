fe1e952e181f2d9e809812e3293f8b4647d8e910
Abhijit Dasgupta <aikidasgupta@gmail.com>
2017-10-25 07:58:46 -0400

    Added links for cohort 5 day 3

--- Cohorts/cohort5day1.py
+++ Cohorts/cohort5day1.py
@@ -213,3 +213,73 @@ A.dot(B.T)
 
 B1 = B.copy()
 B1[0,0 ] = 700
+
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+rng = np.random.RandomState(42)
+rng.rand(10)
+
+x = rng.rand(100) * 5
+
+y = 3 + 2*x + rng.normal(0, 1, 100)
+plt.scatter(x,y)
+
+A = rng.normal(0,1,20)
+A[A>0]
+A[(A < -1) | (A > 1)]
+
+# PyData stack
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+iris = pd.read_csv('iris.csv')
+iris.head()
+type(iris)
+iris.columns
+iris.shape
+iris.describe()
+iris.describe(include='all')
+
+iris['Sepal.Length']
+iris['Sepal.Length'].median()
+iris['Sepal.Length'].plot(kind='hist')
+iris['Sepal.Length'].plot()
+dir(iris['Sepal.Length'])
+help(iris['Sepal.Length'].plot)
+
+iris.dtypes
+iris['Species'].value_counts()
+
+iris.groupby('Species').mean()
+iris.groupby('Species').agg([np.mean, np.std]) 
+
+A = pd.Series([1,np.nan, 2, None])
+A.isnull()
+A[~A.isnull()]
+A[A.notnull()]
+
+A.dropna() # inplace=True makes it stick
+A.fillna(0)
+A.fillna(method='ffill')
+A.fillna(A.mean())
+
+D = pd.DataFrame({'X1': [1,2, None, 4], 'X2': [3, None, 5, 6]})
+D.isnull()
+D.mean()
+D.fillna(D.mean())
+D.fillna({'X1':0, 'X2':100})
+
+D.iloc[:2,0]
+D.loc[0:2, ['X1':'X2']]
+D.index = pd.Index(['a','b','c','d'])
+
+
+
+# To change working directory
+import os
+os.chdir('/Users/abhijit/ARAASTAT/Teaching/FreddieMacFinal/data')
+os.path.join('/Users','abhijit', 'ARAASTAT')
